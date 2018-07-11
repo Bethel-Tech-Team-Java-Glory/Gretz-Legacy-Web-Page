1d0024b4ca4db5012d1c0933c8b44c9fe962a5b1
Celeste Gretz <celeste.gretz@gmail.com>
2018-07-11 15:57:54 -0700

    Added java API Model

---
+++ API/src/main/java/com/gretzlegacy/model/Customer.java
@@ -0,0 +1,99 @@
+package com.gretzlegacy.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name="Customers")
+@EntityListeners(AuditingEntityListener.class)
+
+
+
+
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@NotBlank
+	private String firstName;
+	
+	@NotBlank
+	private String lastName;
+	
+	@NotBlank
+	private String phoneNumber;
+	
+	@NotBlank
+	private String email;
+	
+	@NotBlank
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	private Date createdAt;
+	
+
+}
+
